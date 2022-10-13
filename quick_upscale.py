5c46c5997810670ae59d3e339a68433488b9f156
Extraltodeus <extraltodeus@gmail.com>
2022-10-13 15:40:39 +0200

    Add files via upload

---
+++ quick_upscale.py
@@ -0,0 +1,61 @@
+
+# def run(p, factor):
+#     def simple_upscale(img, factor):
+#         w, h = img.size
+#         w = int(w * factor)
+#         h = int(h * factor)
+#         return img.resize((w, h), Image.LANCZOS)
+#
+#     proc = process_images(p)
+#     basename = ""
+#
+#     for i in range(len(proc.images)):
+#         proc.images[i] = simple_upscale(proc.images[i], factor)
+#         images.save_image(proc.images[i], p.outpath_samples, basename, proc.seed + i, proc.prompt, opts.samples_format, info= proc.info, p=p)
+#
+#     return proc
+# run(p, 2)
+
+import math
+import os
+import sys
+import traceback
+import random
+
+import modules.scripts as scripts
+import modules.images as images
+import gradio as gr
+
+from modules.processing import Processed, process_images
+from PIL import Image
+from modules.shared import opts, cmd_opts, state
+
+class Script(scripts.Script):
+    def title(self):
+        return "Lanczos quick upscale"
+
+    def ui(self, is_img2img):
+        upscale_factor = gr.Slider(minimum=1, maximum=4, step=0.1, label='Upscale factor', value=2)
+        return [upscale_factor]
+
+    def run(self, p, upscale_factor):
+        infotexts = []
+        def simple_upscale(img, upscale_factor):
+            w, h = img.size
+            w = int(w * upscale_factor)
+            h = int(h * upscale_factor)
+            return img.resize((w, h), Image.LANCZOS)
+
+        state.job_count = p.n_iter
+        p.n_iter = 1
+        p.do_not_save_samples = True
+        output_images = []
+        for batch_no in range(state.job_count):
+            proc = process_images(p)
+            infotexts.append(proc.info)
+            proc.images[0] = simple_upscale(proc.images[0], upscale_factor)
+            images.save_image(proc.images[0], p.outpath_samples, "", proc.seed, proc.prompt, opts.samples_format, info= proc.info, p=p)
+            output_images += proc.images
+            p.seed = proc.seed + 1
+
+        return Processed(p, images, infotexts=infotexts,index_of_first_image=0)
